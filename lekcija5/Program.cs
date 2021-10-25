fba4c7ad5bb454fe66a28dfc07037a551212e03d
eeliinaa <elina.sneibe@gmail.com>
2021-10-25 11:58:10 +0300

    init

---
+++ lekcija5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lekcija5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskSum3();
+            //TaskMaxEnd3();
+        }
+
+        static void TaskSum3()
+        {
+            int[] array1 = { 3, 2, 3 };
+            var sum = 0;
+            foreach(var i in array1)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+        static void TaskMaxEnd3()
+        {
+            int[] array1 = { 5, 2, 3 };
+
+            Console.WriteLine("Array:");
+            foreach (var item in array1)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            if (array1.Length > 0)
+            {
+                var arrLength = array1.Length;
+                var arrLargestNr = array1.Max();
+                for (var i = 0; i < arrLength ; i++)
+                {
+                    array1[i] = arrLargestNr;
+                }
+                Console.WriteLine("New array:");
+                foreach (var item in array1)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Array is empty!");
+            }
+        }
+    }
+}
